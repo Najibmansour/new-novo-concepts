44fbbea6bfc3c5ae31c3f4ea0cf44e3eb4ee5771
Najibmansouur <najibmansour2005@gmail.com>
2024-08-08 14:13:27 +0300

    ADD: shader gradient

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import Sec1 from "@/components/homepage/section1";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between "></main>
+    <main className="flex min-h-screen flex-col items-center justify-between ">
+      <Sec1></Sec1>
+    </main>
   );
 }
